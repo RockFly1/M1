6157d5d194b626fc9dfdfe7444ceb4b5ff3885f7
Rock <1525378805@qq.com>
2016-12-02 11:28:52 +0800

    增加头信息

--- src/main/java/com/chinvaventure/m/service/impl/UserServiceImpl.java
+++ src/main/java/com/chinvaventure/m/service/impl/UserServiceImpl.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.chinvaventure.m.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,43 +9,43 @@ import com.chinvaventure.m.service.UserService;
 
 /**
  * user service
- * @author Administrator
  *
+ * @author Administrator
  */
 @Service("userServiceImpl")
 public class UserServiceImpl implements UserService {
 
-	//注入user
-	@Autowired
-	private UserMapper userMapper;
-	
+    //注入user
+    @Autowired
+    private UserMapper userMapper;
 
 
-	@Override
-	public int insertUser(User records) {
-		
-		return userMapper.insertUser(records);
-	}
+    @Override
+    public int insertUser(User records) {
+
+        return userMapper.insertUser(records);
+    }
 
 
-	@Override
-	public User selectByName(String userName, String userPassWord) {
-		
-		return userMapper.selectByName(userName, userPassWord);
-	}
+    @Override
+    public User selectByName(String userName, String userPassWord) {
+
+        return userMapper.selectByName(userName, userPassWord);
+    }
 
 
-	@Override
-	public User selectByEamil(String userEamil) {
-	
-		return userMapper.selectByEamil(userEamil);
-	}
+    @Override
+    public User selectByEamil(String userEamil) {
+
+        return userMapper.selectByEamil(userEamil);
+    }
 
 
-	@Override
-	public int updateUser(User records) {
-		
-		return userMapper.updateUser(records);
-	}
+    @Override
+    public int updateUser(User records) {
+
+        return userMapper.updateUser(records);
+    }
+
 
 }
